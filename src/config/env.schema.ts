0912a29e3c3e1e7cd9b3db1665f7bfaa2ff1353c
Joabesv <joabevarjao123@gmail.com>
2023-02-08 17:40:19 -0300

    feat<secrets>: update secrets

--- src/config/env.schema.ts
+++ src/config/env.schema.ts
@@ -6,9 +6,13 @@ export const configSchema = S.object()
     'HOST',
     'DATABASE_URL',
     'JWT_SECRET',
+    'POSTGRES_USER',
+    'POSTGRES_PASSWORD'
   ])
   .prop('PORT', S.number().default(3000))
   .prop('HOST', S.string().default('localhost'))
+  .prop('POSTGRES_PASSWORD', S.string())
+  .prop('POSTGRES_USER', S.string().minLength(5))
   .prop('DATABASE_URL', S.string())
   // ask for review about Secret possible types
   .prop('JWT_SECRET', S.string());
