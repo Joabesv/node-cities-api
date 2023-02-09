06936bb8555e8da9b602533dc45cf49371e0efc2
Joabesv <joabevarjao123@gmail.com>
2023-02-08 23:28:08 -0300

    feat<route>: implement first api route

---
+++ src/modules/root.ts
@@ -0,0 +1,14 @@
+import { FastifyPluginAsync } from 'fastify';
+
+const root: FastifyPluginAsync = async (fastify, opts) => {
+  fastify.get('/', async (request, reply) => {
+    try {
+      fastify.log.info('this is working, root route')
+      return reply.code(200).send({ msg: 'be welcome guest' })
+    } catch (error: unknown) {
+      fastify.log.error(error, 'error in root route')
+    }
+  })
+};
+
+export default root;
